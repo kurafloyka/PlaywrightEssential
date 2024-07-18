60d9599e780b891b09663d7b0d9d161ccf79994b
Faruk <faruk.akyol@consultant.boyner.com.tr>
2024-07-18 10:19:07 +0300

    first commit

---
+++ src/test-data.ts
@@ -0,0 +1,10 @@
+interface TestData {
+    testUrl: string;
+    title: string;
+}
+
+export const testdata: TestData = {
+    testUrl: "https://www.amazon.com.tr",
+    title: "Amazon.com.tr: Elektronik, bilgisayar, akıllı telefon, kitap, oyuncak, yapı market, ev, mutfak, oyun konsolları ürünleri ve daha fazlası için internet alışveriş sitesi"
+
+};
---
+++ tests/amazon-listing-product.spec.ts
@@ -0,0 +1,20 @@
+import test, { expect } from "@playwright/test";
+import { testdata } from "../src/test-data";
+
+test.describe("Amazon Testing", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(testdata.testUrl);
+    //await console.log(await page.title());
+    await expect(page).toHaveTitle(testdata.title, { timeout: 5000 });
+  });
+  test("Amazon Listing Product", async ({ page }) => {
+    await page
+      .getByRole("link", { name: "Merhaba, Giriş yapın Hesap ve" })
+      .waitFor({ state: "visible", timeout: 5000 });
+    await page
+      .getByRole("link", { name: "Merhaba, Giriş yapın Hesap ve" })
+      .click();
+    await expect.soft(page).toHaveURL(/signin/);
+    await page.pause();
+  });
+});
