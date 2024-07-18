0773f2bf0604062590ee2bd64d89402e8ebd9b0a
Faruk <faruk.akyol@consultant.boyner.com.tr>
2024-07-18 12:04:19 +0300

    Fixed html report problem

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,11 +17,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 6 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['list'],['allure-playwright',{outputFolder: 'test-results'}]],
+  reporter: [['html',{outputFolder: 'test-results/'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -29,6 +29,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: "on",
   },
 
   /* Configure projects for major browsers */
