df801d382b5532488851e2cb72ba1aaca8ec34da
Faruk <faruk.akyol@consultant.boyner.com.tr>
2024-07-21 22:28:14 +0300

    Renamed test cases

--- tests/api-crud-testing.spec.ts
+++ tests/api-crud-testing.spec.ts
@@ -1,5 +1,4 @@
 import test, { expect } from "@playwright/test";
-import { faker } from "@faker-js/faker";
 import userController from "../src/controller/user-controller";
 import { data, data2 } from "../config/base-config";
 
@@ -7,15 +6,15 @@ test.describe("API CRUD Testing", () => {
   let createUser;
   let createUser2;
 
-  test.beforeAll(async ({ playwright }) => {
+  test.beforeAll(async ({ request }) => {
     await userController.UserController();
   });
 
-  test("List All Users", async ({ browser }) => {
+  test("List Users", async ({ request }) => {
     await userController.listUsers();
   });
 
-  test("CRUD user", async ({ browser }) => {
+  test("Create User", async ({ request }) => {
     const responseCreate = await userController.createUser(data);
     console.log("Response Body : ....." + (await responseCreate.body()));
     expect(responseCreate.status()).toBe(201);
@@ -31,7 +30,7 @@ test.describe("API CRUD Testing", () => {
     expect(responseJson).toHaveProperty("status");
   });
 
-  test("Update User", async ({ browser }) => {
+  test("Update User", async ({ request }) => {
     createUser = await userController.createUser(data);
     //console.log(await createUser.json());
     const createUserJson = await createUser.json();
@@ -48,7 +47,7 @@ test.describe("API CRUD Testing", () => {
     console.log("Updated User Full Name: " + (await updatedUserJson.name));
   });
 
-  test("Delete User", async ({ browser }) => {
+  test("Delete User", async ({ request }) => {
     createUser2 = await userController.createUser(data);
     const createUser2Json = await createUser2.json();
     console.log("Created User ID: " + (await createUser2Json.id));
