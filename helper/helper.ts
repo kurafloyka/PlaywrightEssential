35ff80d60c6592dc7d06b8f9629a19e440e42bcc
Faruk <faruk.akyol@consultant.boyner.com.tr>
2024-07-21 22:20:34 +0300

    Added helper method

---
+++ helper/helper.ts
@@ -0,0 +1,8 @@
+import userController from "../src/controller/user-controller";
+
+export const login = async (email: string, password: string) => {
+  const body = { email: email, password: password };
+  const res = await userController.listUsers();
+  //return res.body.token;
+  //let  token = await login(config.email, config.password);
+};
